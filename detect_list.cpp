191225531ddfa6058063f7a290e3af34968224ed
issunyi <issunsun1@gmail.com>
2024-06-13 19:36:14 +0800

    20240613

---
+++ detect_list.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <stack>
+#define maxsize 1000
+
+using namespace std;
+
+int find_math(vector<int> &a)
+{
+
+    return 1;
+}
+
+int main()
+{
+
+    return 0;
+}
